25ee92167435af3768049e3f65b6161a8f7723c0
musicfreakt <yalovega.nikita5@gmail.com>
2018-09-03 18:17:37 +0500

    Add some fixes v.2

--- test_app.cpp
+++ test_app.cpp
@@ -2,30 +2,30 @@
 
 Turtle turtle;
 
-void koch(int a, int n){
+void draw_koch(int a, int n){
   if(n == 0){
     turtle.move(a);
     return;
   }
-  koch(a/3,n-1);
+  draw_koch(a/3,n-1);
   turtle.turn_left(60);
-  koch(a/3,n-1);
+  draw_koch(a/3,n-1);
   turtle.turn_right(120);
-  koch(a/3,n-1);
+  draw_koch(a/3,n-1);
   turtle.turn_left(60);
-  koch(a/3,n-1);
+  draw_koch(a/3,n-1);
 }
 
 int main() {
   turtle.pen_down();
   turtle.color(0.19,0.85, 0.39);
-  turtle.move_to(150,150);
+  turtle.move_to(250,250);
 
-  koch(100, 2);
-  turtle.turn_right(120);
-  koch(100, 2);
-  turtle.turn_right(120);
-  koch(100, 2);
+  turtle.turn_right(30);
+  for (int i = 0; i < 3; i++) {
+    draw_koch(200, 3);
+    turtle.turn_right(120);
+  }
 
   return 0;
 }
