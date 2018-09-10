9fa516c623ad7a8f9ecb55a17788d9700e1619ce
musicfreakt <yalovega.nikita5@gmail.com>
2018-09-10 15:57:12 +0500

    Fix output file

--- turtle.cpp
+++ turtle.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 using namespace std;
 
-ofstream psfile ("output.ps"); // открываем файл, в котором будет рисунок
+ofstream temp_file ("tmp.ps"); // временный файл для сохранения картинки
 
 #include "turtle.h"
 
 Turtle::Turtle(){
   /* Конструктор */
-  psfile << "1 setlinecap" << endl;
-  psfile << "1 setlinejoin" << endl;
+  temp_file << "1 setlinecap" << endl;
+  temp_file << "1 setlinejoin" << endl;
 }
 
 // --------------- Методы передвижения ---------------
@@ -74,14 +74,14 @@ void Turtle::drawCircle(float rad){
    *  в месте, в котором стоит черепаха.
    */
   if (pen)
-    psfile << "newpath " << x << " " << y << " " << rad << " 0 360 arc stroke" << endl;
+    temp_file << "newpath " << x << " " << y << " " << rad << " 0 360 arc stroke" << endl;
 }
 
 void Turtle::drawCircle(float rad, float angle){
   /*  Рисует дугу радиуса rad с углом angle */
   if (pen){
     if (angle > 0 && angle <= 360)
-      psfile << "newpath " << x << " " << y << " " << rad << " 0 " << angle << " arc stroke" << endl;
+      temp_file << "newpath " << x << " " << y << " " << rad << " 0 " << angle << " arc stroke" << endl;
   }
 }
 
@@ -89,32 +89,32 @@ void Turtle::drawCircle(float rad, float start, float end){
   /*  Рисует дугу радиуса rad от start до end */
   if (pen){
     if (start >= 0 && start < end && end >= 360)
-      psfile << "newpath " << x << " " << y << " " << rad << " " << start << " " << end << " arc stroke" << endl;
+      temp_file << "newpath " << x << " " << y << " " << rad << " " << start << " " << end << " arc stroke" << endl;
   }
 }
 
 void Turtle::drawLine(float x1, float y1){
   /* Рисует путь черепахи, если перо опущенно */
-  psfile << "newpath " << x << " " << y <<" moveto "<< x1 << " " << y1 <<" lineto stroke"<<endl;
+  temp_file << "newpath " << x << " " << y <<" moveto "<< x1 << " " << y1 <<" lineto stroke"<<endl;
 }
 
 void Turtle::setColor(double red, double green, double blue){
   /* Смена цвета пера черепахи  */
   if (red <= 1 && green <= 1 && blue <= 1){
-    psfile << red << " " << green << " " << blue << " setrgbcolor"<< endl;
+    temp_file << red << " " << green << " " << blue << " setrgbcolor"<< endl;
   }
 }
 
 void Turtle::setColor(int red, int green, int blue){
   /* Смена цвета пера черепахи */
   if (red <= 255 && green <= 255 && blue <= 255) {
-    psfile << (float)red / 255 << " " << (float)green / 255 << " " << (float)blue / 255 << " setrgbcolor"<< endl;
+    temp_file << (float)red / 255 << " " << (float)green / 255 << " " << (float)blue / 255 << " setrgbcolor"<< endl;
   }
 }
 
 void Turtle::setWidth(int width){
   /* Изменяем ширину линии */
-  psfile << width << " setlinewidth" << endl;
+  temp_file << width << " setlinewidth" << endl;
 }
 
 void Turtle::setPictureSize(){
@@ -127,11 +127,17 @@ void Turtle::setPictureSize(){
 
 Turtle::~Turtle(){
   /* Деконструктор */
-  psfile << "showpage" <<endl;
-  psfile.seekp(0, std::ios::beg);
-  psfile << "%!PS-Adobe-3.0 EPSF-3.0\n";
-  psfile << "%%BoundingBox: "<< round(picture_size[0][0]) << " " << round(picture_size[0][1]) << " " << round(picture_size[1][0]) << " " << round(picture_size[1][1]) << "\n";
-  psfile << "%%HiResBoundingBox: "<< picture_size[0][0] << " " << picture_size[0][1] << " " << picture_size[1][0] << " " << picture_size[1][1] << "\n";
-  psfile << "%%EndComments" <<endl;
-  psfile.close(); // закрываем файл
+  temp_file << "showpage" <<endl;
+  temp_file.close();
+  ifstream temp_file ("tmp.ps");
+  ofstream output_file ("output.ps");
+  output_file << "%!PS-Adobe-3.0 EPSF-3.0\n";
+  output_file << "%%BoundingBox: "<< round(picture_size[0][0]) << " " << round(picture_size[0][1]) << " " << round(picture_size[1][0]) << " " << round(picture_size[1][1]) << "\n";
+  output_file << "%%HiResBoundingBox: "<< picture_size[0][0] << " " << picture_size[0][1] << " " << picture_size[1][0] << " " << picture_size[1][1] << "\n";
+  output_file << "%%EndComments" <<endl;
+  output_file << temp_file.rdbuf();
+  // закрываем файлы
+  temp_file.close();
+  output_file.close();
+  remove("tmp.ps"); // удаляем временный файл
 }
