e75b61327528abb5de345a17120dc0c9658a7d92
musicfreakt <yalovega.nikita5@gmail.com>
2018-10-08 00:39:25 +0500

    Move libs to new path

--- example.cpp
+++ example.cpp
@@ -3,7 +3,7 @@
 * to draw Sierpinski triangle
 */
 
-#include "LSystem.cpp"
+#include "libs/LSystem.cpp"
 #include <string>
 #include <map>
 
@@ -15,9 +15,9 @@ map<char, string> rules = {{'F', "F-G+F+G-F"}, {'G', "GG"}};
 map<char, string> inter = {{'F', "move"}, {'+', "turnLeft"}, {'-', "turnRight"}, {'G', "move"}};
 
 int main() {
-  LSystem lsystem("F-G-G", rules);
-  lsystem.setColor(120, 168, 93);
-  lsystem.iter(5);
-  lsystem.interpret(inter, step, angle);
+  LSystem sierpinski("F-G-G", rules);
+  sierpinski.setColor(120, 168, 93);
+  sierpinski.iter(5);
+  sierpinski.interpret(inter, step, angle);
   return 0;
 }
