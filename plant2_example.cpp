c7d610701f1166073bc9422af53002f5e9444e88
musicfreakt <yalovega.nikita5@gmail.com>
2019-04-18 18:54:48 +0500

    merge master

--- plant2_example.cpp
+++ plant2_example.cpp
@@ -2,14 +2,13 @@
 
 using namespace std;
 
-// vector<string> rules = {"F => F[+F]F[-F][F] (0.5)", "F => F[+F]F (0.3)", "F => F[-F]F (0.2)"};
-vector<string> rules = {"X => F+[[X]-X]-F[-FX]+X", "F => FF"};
+vector<string> rules = {"F => F[+F]F[-F][F] (0.5)", "F => F[+F]F (0.3)", "F => F[-F]F (0.2)"};
 
 int main(int argc, char *argv[]) {
   LSystem plant;
   plant.setStep(5.0);
   plant.setAngle(25.0);
-  plant.build("X", rules, 6);
+  plant.build("F", rules, 6);
   plant.loop();
   return 0;
 }
